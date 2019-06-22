3f285c5c6167d07ef3f9e2679114bac7e9fba41c
sonalnanodkar <51533840+sonalnanodkar@users.noreply.github.com>
2019-06-22 14:04:37 +0530

    Add files via upload

---
+++ TestApplication/TestApplication/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+	public class Enums
+	{
+		public enum StatusCodes
+		{
+			initial = 0,
+			fileCreated = 1,
+			waiting = 2,
+			failure = 3,
+			success = 4
+		}
+
+		public enum FileType
+		{
+			add_user = 0,
+			add_account = 1,
+			assign_account = 2
+		}
+	}
+}
